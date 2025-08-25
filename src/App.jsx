d653351d8c7ef03d5ff5778356f3fc9dd6dd81b2
SOHORABHOSSAINTAREK <sh.tarek.9123@gmail.com>
2025-08-25 23:24:45 +0600

    Setup Done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='sora-font'>
+        <h1 className='text-5xl '>Vite + React</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
